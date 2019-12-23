f676d8dca4f0dcf6e08d8e9c48315b7ddba72a9a
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2019-12-23 21:34:15 +0000

    Added linter with air-bnb present and updated files to match rules
     Changes to be committed:
    	modified:   .babelrc
    	new file:   .eslintignore
    	new file:   .eslintrc.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/configuration.js
    	modified:   src/functionName.js

--- src/configuration.js
+++ src/configuration.js
@@ -1,36 +1,35 @@
-const aws = require("aws-sdk");
+import aws from 'aws-sdk';
 
-const REGION = process.env.REGION;
+const { REGION } = process.env;
 const isRunningLocally = process.env.AWS_SAM_LOCAL === 'true';
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': 'content-type, Authorization'
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Allow-Headers': 'content-type, Authorization',
 };
 
 const environment = {
 };
 
 if (isRunningLocally) {
-    aws.config.update({
-        region: "local",
-        endpoint: "http://dynamodb:8000/"
-    });
-    // Provide Local versions when running locally
-    // environment.EXAMPLE_TABLE_NAME = 'exampleTableName';
-
+  aws.config.update({
+    region: 'local',
+    endpoint: 'http://dynamodb:8000/',
+  });
+  // Provide Local versions when running locally
+  // environment.EXAMPLE_TABLE_NAME = 'exampleTableName';
 } else {
-    aws.config.update({
-        region: REGION
-    });
+  aws.config.update({
+    region: REGION,
+  });
 }
 
 const docClient = new aws.DynamoDB.DocumentClient();
 
 export {
-    aws,
-    docClient,
-    corsHeaders,
-    environment
+  aws,
+  docClient,
+  corsHeaders,
+  environment,
 };
--- src/functionName.js
+++ src/functionName.js
@@ -1,12 +1,14 @@
-import { corsHeaders } from "./configuration";
+import { corsHeaders } from './configuration';
 
-export const handler = async function (event, context) {
-    console.log('I Ran!');
+export const handler = async (event, context) => {
+  console.log('I Ran!');
+  console.log(JSON.stringify(event));
+  console.log(JSON.stringify(context));
 
-    return {
-        headers: corsHeaders,
-        body: {"cake": "good"}
-    };
+  return {
+    headers: corsHeaders,
+    body: { cake: 'good' },
+  };
 };
 
 export default {};
