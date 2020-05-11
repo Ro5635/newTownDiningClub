6f91a0704dc6f1ebc246bb874ed9b146a2ea01de
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2020-05-11 15:34:05 +0100

    Added Apply vendorState Domain Service

---
+++ src/domain/service/vendor/apply_vendor_state.js
@@ -0,0 +1,8 @@
+
+const applyVendorStateDomainService = async ({ vendor, newVendorState }) => {
+  const { vendorState, ...unModifiedVendorFields } = vendor;
+  const newVendor = { ...unModifiedVendorFields, vendorState: newVendorState };
+  return { vendor: newVendor };
+};
+
+export default applyVendorStateDomainService;
