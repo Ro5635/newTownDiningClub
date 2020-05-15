a774e489819b8bb48a0f0224f7033ab4cea74b52
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2020-05-15 22:25:25 +0100

    Added New Message Attributes to Read Message Repository

--- src/adapters/persistence/message_repository/read_message.js
+++ src/adapters/persistence/message_repository/read_message.js
@@ -1,48 +1,48 @@
 import {
-    logger, errors, docClient, environment,
+  logger, errors, docClient, environment,
 } from '../../../infrastructure/configuration';
 
 const { NonExistentItemError } = errors;
 const { NTDC_TABLE_NAME } = environment;
 
 const itemToMessage = (messageItem) => {
-    const {
-        PK, SK, 'GSI1-PK': GSI1PK, 'GSI1-SK': GSI1SK, ...message
-    } = messageItem;
-    return { ...message };
+  const {
+    PK, SK, 'GSI1-PK': GSI1PK, 'GSI1-SK': GSI1SK, ...message
+  } = messageItem;
+  return { ...message };
 };
 
 const readMessage = async ({ messageId }) => {
-    try {
-        logger.info(`Attempting to read messageId:${messageId} from DB`);
+  try {
+    logger.info(`Attempting to read messageId:${messageId} from DB`);
 
-        const params = {
-            TableName: NTDC_TABLE_NAME,
-            Key: {
-                PK: `Message_${messageId}`,
-                SK: 'MessageType_Email',
-            },
-            Limit: 1,
-            ReturnConsumedCapacity: 'TOTAL',
-        };
+    const params = {
+      TableName: NTDC_TABLE_NAME,
+      Key: {
+        PK: `Message_${messageId}`,
+        SK: 'messageType_email',
+      },
+      Limit: 1,
+      ReturnConsumedCapacity: 'TOTAL',
+    };
 
-        const { Item, ConsumedCapacity } = await docClient.get(params).promise();
-        logger.info(`DynamoDB get succeeded with response: ${JSON.stringify(ConsumedCapacity)}`);
-        if (!Item) throw new NonExistentItemError('No Matching MessageItem Found In DB');
-        const message = itemToMessage(Item);
-        return {
-            message,
-        };
-    } catch (error) {
-        if (error instanceof NonExistentItemError) {
-            logger.info('Unable to get the requested resource from the DB');
-            throw error;
-        }
-
-        logger.error('failed to get Message from persistence');
-        logger.error(error);
-        throw new Error('Failed to read Message');
+    const { Item, ConsumedCapacity } = await docClient.get(params).promise();
+    logger.info(`DynamoDB get succeeded with response: ${JSON.stringify(ConsumedCapacity)}`);
+    if (!Item) throw new NonExistentItemError('No Matching MessageItem Found In DB');
+    const message = itemToMessage(Item);
+    return {
+      message,
+    };
+  } catch (error) {
+    if (error instanceof NonExistentItemError) {
+      logger.info('Unable to get the requested resource from the DB');
+      throw error;
     }
+
+    logger.error('failed to get Message from persistence');
+    logger.error(error);
+    throw new Error('Failed to read Message');
+  }
 };
 
 export default readMessage;
